6e0f91cfb0310c892fcfaddb5d2e5e6db3299c91
sankalp-singh-07 <sankalp.singh.somvanshi@gmail.com>
2024-12-30 22:31:03 +0530

    schemas for recipe and author created. sanity markdown plugin installed.

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
-import { type SchemaTypeDefinition } from 'sanity'
+import { type SchemaTypeDefinition } from 'sanity';
+import { author } from './author';
+import { recipe } from './recipe';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
-}
+	types: [author, recipe],
+};
