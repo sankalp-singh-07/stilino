260fbf9b2c650ab3cf6e557b7f2d267f2ad3b17b
sankalp-singh-07 <sankalp.singh.somvanshi@gmail.com>
2024-12-31 15:29:23 +0530

    live data fetching using live.ts

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,7 @@
 import ItemCard, { ItemCardType } from '@/components/ItemCard';
 import Searchbar from '@/components/Searchbar';
-import { client } from '@/sanity/lib/client';
+// import { client } from '@/sanity/lib/client';
+import { sanityFetch, SanityLive } from '@/sanity/lib/live';
 import { RECIPES_QUERY } from '@/sanity/lib/queries';
 
 export default async function Home({
@@ -10,36 +11,41 @@ export default async function Home({
 }) {
 	const query = (await searchParams).query;
 
-	const posts = await client.fetch(RECIPES_QUERY);
+	// const posts = await client.fetch(RECIPES_QUERY);
+
+	const { data: posts } = await sanityFetch({ query: RECIPES_QUERY });
 
 	// console.log(JSON.stringify(posts));
 
 	return (
-		<div className="ml-4 mr-4 mt-3 mb-6 ">
-			<div className="pink_container">
-				<h1 className="heading">Cook. Share. Inspire.</h1>
-				<p className="sub-heading">
-					Find, create, and share your favorite recipes with the
-					world.
-				</p>
-				<Searchbar query={query} />
+		<>
+			<div className="ml-4 mr-4 mt-3 mb-6 ">
+				<div className="pink_container">
+					<h1 className="heading">Cook. Share. Inspire.</h1>
+					<p className="sub-heading">
+						Find, create, and share your favorite recipes with the
+						world.
+					</p>
+					<Searchbar query={query} />
+				</div>
+				<div className="section_container">
+					<p className="text-2xl text-black font-semibold">
+						{query
+							? `Search results for "${query}"`
+							: 'Our Top Recipes'}
+					</p>
+					<ul className="mt-6 card_grid">
+						{posts?.length > 0 ? (
+							posts.map((post: ItemCardType) => (
+								<ItemCard key={post._id} post={post} />
+							))
+						) : (
+							<p className="no-results">No items found 😅</p>
+						)}
+					</ul>
+				</div>
 			</div>
-			<div className="section_container">
-				<p className="text-2xl text-black font-semibold">
-					{query
-						? `Search results for "${query}"`
-						: 'Our Top Recipes'}
-				</p>
-				<ul className="mt-6 card_grid">
-					{posts?.length > 0 ? (
-						posts.map((post: ItemCardType) => (
-							<ItemCard key={post._id} post={post} />
-						))
-					) : (
-						<p className="no-results">No items found 😅</p>
-					)}
-				</ul>
-			</div>
-		</div>
+			<SanityLive />
+		</>
 	);
 }
