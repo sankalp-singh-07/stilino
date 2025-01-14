15596f33759e8b551bdcf6813e7bc2224f1083b6
sankalp-singh-07 <sankalp.singh.somvanshi@gmail.com>
2025-01-15 01:34:20 +0530

    Redesigning the footer section.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,7 @@
 'use client';
+import { FormEvent, useState } from 'react';
 
-const Footer = () => {
+const Footer = ({ message }: { message: string }) => {
 	// const [email, setEmail] = useState('');
 
 	// const handleSubmit = (e: FormEvent) => {
@@ -16,11 +17,14 @@ const Footer = () => {
 	// };
 
 	return (
-		<div className="ml-4 mr-4 mt-3 mb-6">
-			<div className="w-full bg-primary md:min-h-[360px] min-h-[280px] flex justify-center items-center flex-col md:py-10 py-3 px-3 rounded-lg">
-				<h1 className="px-4 py-2 tracking-tighter font-semibold text-whiteBg text-[32px] md:text-[72px] sm:text-[48px] max-w-4xl text-center my-4 sm:leading-[48px] leading-[30px]">
+		<div className="ml-4 mr-4 mt-2 mb-6">
+			<div className="w-full bg-primary h-fit text-white flex  items-center md:py-10 py-8 px-8  md:px-12 rounded-lg  justify-between">
+				<p className="text-sm md:text-base">Stilino. Made in 2025.</p>
+				<p className="text-sm md:text-base">Dev 👋</p>
+
+				{/* <h1 className="px-4 py-2 tracking-tighter font-semibold text-whiteBg text-[32px] md:text-[72px] sm:text-[48px] max-w-4xl text-center my-4 sm:leading-[48px] leading-[30px]">
 					STILINO.
-				</h1>
+				</h1> */}
 				{/* <div className="w-full flex items-center justify-center flex-col">
 					<p className="font-normal text-sm sm:text-base text-white max-w-2xl text-center">
 						{message}
@@ -45,12 +49,9 @@ const Footer = () => {
 						</button>
 					</form>
 				</div> */}
-				<div className="border-t-2 border-tertiary border-opacity-10 mt-auto flex justify-between w-full px-3 md:px-12 py-3 text-white text-base font-medium">
-					<p className="text-sm md:text-base">
-						Stilino. Made in 2025.
-					</p>
-					<p className="text-sm md:text-base">Dev 👋</p>
-				</div>
+				{/* <div className="border-t-2 border-tertiary border-opacity-10 mt-auto flex justify-between w-full px-3 md:px-12 py-3 text-white text-base font-medium">			 */}
+
+				{/* </div> */}
 			</div>
 		</div>
 	);
